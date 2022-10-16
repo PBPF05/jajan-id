76782c0f16eca28a8036b3922181826a29d1f0e9
Rendy Arya Kemal <renrror@gmail.com>
2022-10-16 12:47:42 +0700

    Register stuffs to django-admin

--- detail/admin.py
+++ detail/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from detail.models import Barang, JadwalOperasi
+
 # Register your models here.
+admin.register(Barang)
+admin.register(JadwalOperasi)
--- katalog/admin.py
+++ katalog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from katalog.models import Toko
+
 # Register your models here.
+admin.register(Toko)
