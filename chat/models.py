cedf3440685eb32b03564d80a8b6dc97917f1f8e
Rendy Arya Kemal <renrror@gmail.com>
2022-10-25 21:44:36 +0700

    Separate channel with messages

--- chat/models.py
+++ chat/models.py
@@ -2,13 +2,26 @@ from django.db import models
 from katalog.models import Toko
 from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Channel(models.Model):
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["toko", "user"],
+                name="toko_user_uq",
+            )
+        ]
+        indexes = [models.Index(fields=["toko", "user"], name="toko_user_idx")]
+
+    toko = models.ForeignKey(Toko, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
 class Pesan(models.Model):
     class Pengirim(models.TextChoices):
         pengirim = "pengirim"
         user = "user"
 
-    toko = models.ForeignKey(Toko, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     pesan = models.CharField(max_length=256)
     pengirim = models.CharField(max_length=8, choices=Pengirim.choices)
+    channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
