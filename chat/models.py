45df05b3e06f61df5a08380002703a5859a85bb6
Rendy Arya Kemal <renrror@gmail.com>
2022-10-26 17:23:14 +0700

    Add type hint for relation

--- chat/models.py
+++ chat/models.py
@@ -1,6 +1,10 @@
+from typing import TYPE_CHECKING
 from django.db import models
 from katalog.models import Toko
 from django.contrib.auth.models import User
+
+if TYPE_CHECKING:
+    from django.db.models.manager import RelatedManager
 
 
 class Channel(models.Model):
@@ -15,6 +19,8 @@ class Channel(models.Model):
 
     toko = models.ForeignKey(Toko, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    pesan_set: "RelatedManager[Pesan]"
 
 
 class Pesan(models.Model):
