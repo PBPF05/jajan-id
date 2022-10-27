10f70b83883e31e6bd37210e7f5b63722d866204
Ren <renrror@gmail.com>
2022-10-27 12:58:44 +0700

    Show load more btn

--- static/js/chat.js
+++ static/js/chat.js
@@ -3,6 +3,7 @@
 // CONSTANTS
 const LOAD_MORE_HTML = `<button class="btn btn-primary me-auto ms-auto">Load More</button>`;
 const IS_SELLER = window.location.href.indexOf("user") != -1;
+let showLoadMoreBtn = false;
 let messages = [];
 
 function getMessages(beforeId, afterId, cb) {
@@ -14,7 +15,10 @@ function getMessages(beforeId, afterId, cb) {
   if (beforeId) requestUrl.searchParams.set("before", beforeId);
   if (afterId) requestUrl.searchParams.set("after", afterId);
 
-  $.getJSON(requestUrl.toString(), cb);
+  $.getJSON(requestUrl.toString(), (data) => {
+    showLoadMoreBtn = data.length == 50
+    cb(data)
+  });
 }
 
 function drawChat() {
@@ -22,6 +26,16 @@ function drawChat() {
   chatArea.innerHTML = "";
 
   messages.map((message) => {
+    const $loadMoreBtn = $(LOAD_MORE_HTML)
+    $loadMoreBtn.click(() => {
+      getMessages(messages[0].pk, null, (data) => {
+        messages = [...data, ...messages]
+        drawChat()
+      })
+    })
+
+    if (showLoadMoreBtn) $(chatArea).append($loadMoreBtn)
+
     const newDiv = document.createElement("div");
     const newContent = document.createTextNode(message.fields.pesan);
 
@@ -61,7 +75,7 @@ function submitChat(e) {
   });
 }
 
-$(function () {
+$(function() {
   getMessages(null, null, (data) => {
     messages = data;
     drawChat();
