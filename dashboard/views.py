ddfe0a1f2795d049ef127f118f473047ebc83eb5
Divany <harryndira@gmail.com>
2022-10-28 22:50:44 +0700

    Working on Dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from katalog.models import Toko
+from django.core import serializers
+from django.http import HttpResponse
 # Create your views here.
+
+def show_dashboard(request):
+    data_toko = Toko.objects.filter(pemilik = request.user)
+    return render(request, 'dashboard.html')
+
+def show_data_json(request):
+    data_toko = Toko.objects.filter(pemilik = request.user)
+    return HttpResponse(serializers.serialize("json", data_toko))
