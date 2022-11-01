b9a4f1fbacf6398814bf28261b82e5b5bde6bf8a
Divany <harryndira@gmail.com>
2022-11-02 00:53:47 +0700

    Add Class Review

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from katalog.models import Toko
 # Create your models here.
 class Review:
-    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    komentator = models.ForeignKey(User, on_delete = models.CASCADE)
     komentar = models.TextField()
+    toko = models.ForeignKey(Toko, on_delete = models.CASCADE)
