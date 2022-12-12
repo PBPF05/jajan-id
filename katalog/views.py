2ad498e7b69bf4e966a21b200b15797301727ee5
tasyaashil <93830651+tasyaashil@users.noreply.github.com>
2022-12-12 14:31:13 +0700

    Update views.py

--- katalog/views.py
+++ katalog/views.py
@@ -5,6 +5,7 @@ from django.http.response import HttpResponse, HttpResponseNotFound
 from django.http import HttpResponseRedirect
 from django.core import serializers
 import json
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
@@ -28,3 +29,8 @@ def show_json(request):
 def show_json_search(request, nama_toko):
     status = Toko.objects.filter(nama__icontains=nama_toko)
     return HttpResponse(serializers.serialize('json', status))
+
+@csrf_exempt
+def show_json_search_flutter(request, nama_toko):
+    status = Toko.objects.filter(nama__icontains=nama_toko)
+    return JsonResponse({'message': 'success'})
